cacfaaa1586f2559564f5a1257b88d7cad37604f
Dmytro Diachenko <dmytro.diachenko@loopme.com>
2022-02-27 12:27:41 +0200

    create components

---
+++ lesson_1/src/components/App.js
@@ -0,0 +1,11 @@
+import { Header } from "./header/Header";
+import { Main } from "./main/Main";
+import { Footer } from "./footer/Footer";
+
+export function App() {
+  return <>
+    <Header/>
+    <Main/>
+    <Footer/>
+  </>
+}
