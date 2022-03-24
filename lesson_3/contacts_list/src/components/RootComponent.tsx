bcc9788b6f10a519370cd7badca642b51bd00448
Dmytro Diachenko <dmytro.diachenko@loopme.com>
2022-03-24 21:12:03 +0200

    inited progect

---
+++ lesson_3/contacts_list/src/components/RootComponent.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react';
+
+class RootComponent extends React.Component {
+    render(): ReactElement {
+        return <></>
+    }
+}
+
+export { RootComponent }
