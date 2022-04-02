ea9cefa0f15810967f951ca153e6ee9cbdc7bc5c
Dmytro Diachenko <dmytro.diachenko@loopme.com>
2022-04-02 12:43:45 +0300

    added fonts, rewrote list & itemList to fn components

--- lesson_4/fn_contact_list/src/components/itemListContact/ItemListContact.tsx
+++ lesson_4/fn_contact_list/src/components/itemListContact/ItemListContact.tsx
@@ -1,6 +1,6 @@
-import React, { ReactElement } from 'react';
+import { ReactElement } from 'react';
 
-type TItemContactList = {
+type TPropsItemContactList = {
     name: string,
     surname: string,
     phone: string,
@@ -10,36 +10,34 @@ type TItemContactList = {
     getDataItem: Function
 }
 
-class ItemListContact extends React.Component<TItemContactList> {
-    render(): ReactElement {
-        const {
-            name,
-            surname,
-            phone,
-            id,
-            deleteContact,
-            stateViewForm,
-            getDataItem
-        } = this.props as TItemContactList;
-        return (
-            <li id={id}>
-                <p className="name">{ name }</p>
-                <p className="surname">{ surname }</p>
-                <p className="phone">{ phone }</p>
-                <div className="buttons__item">
-                    <button onClick={(): void => { deleteContact(id) }}>delete</button>
-                    <button onClick={
-                        (): void => {
-                            stateViewForm('update');
-                            getDataItem(name, surname, phone, id);
-                        }
-                    }>
-                        edit
-                    </button>
-                </div>
-            </li>
-        )
-    }
+const ItemListContact = (props: TPropsItemContactList): ReactElement => {
+    const {
+        name,
+        surname,
+        phone,
+        id,
+        deleteContact,
+        stateViewForm,
+        getDataItem
+    } = props as TPropsItemContactList;
+    return (
+        <li id={id}>
+            <p className="name">{ name }</p>
+            <p className="surname">{ surname }</p>
+            <p className="phone">{ phone }</p>
+            <div className="buttons__item">
+                <button onClick={(): void => { deleteContact(id) }}>delete</button>
+                <button onClick={
+                    (): void => {
+                        stateViewForm('update');
+                        getDataItem(name, surname, phone, id);
+                    }
+                }>
+                    edit
+                </button>
+            </div>
+        </li>
+    )
 }
 
 export { ItemListContact }
--- lesson_4/fn_contact_list/src/components/listContact/ListContact.tsx
+++ lesson_4/fn_contact_list/src/components/listContact/ListContact.tsx
@@ -22,49 +22,47 @@ type TPropsListContact = {
     resetUpdateData: Function
 }
 
-class ListContact extends React.Component<TPropsListContact> {
-    render(): ReactElement {
-        const {
-            contacts,
-            stateViewForm,
-            deleteContact,
-            getDataItem,
-            resetUpdateData
-        } = this.props as TPropsListContact;
-        return <div className="ListContact__wrapper">
-            <div className="title">
-                <p>List of contacts</p>
-            </div>
-            <ul className="list__contacts">
-                {
-                    contacts.map((item: TItemContactList): ReactElement => {
-                        const { name, surname, phone, id } = item as TItemContactList;
-                        return <ItemListContact
-                            key={id}
-                            name={name}
-                            surname={surname}
-                            phone={phone}
-                            id={id}
-                            deleteContact={deleteContact}
-                            stateViewForm={stateViewForm}
-                            getDataItem={getDataItem}
-                        />
-                    })
-                }
-            </ul>
-            <button
-                className="add__contact"
-                onClick={
-                    (): void => {
-                        stateViewForm('add');
-                        resetUpdateData();
-                    }
-                }
-            >
-                add contact
-            </button>
+const ListContact = (props: TPropsListContact): ReactElement => {
+    const {
+        contacts,
+        stateViewForm,
+        deleteContact,
+        getDataItem,
+        resetUpdateData
+    } = props as TPropsListContact;
+    return <div className="ListContact__wrapper">
+        <div className="title">
+            <p>List of contacts</p>
         </div>
-    }
+        <ul className="list__contacts">
+            {
+                contacts.map((item: TItemContactList): ReactElement => {
+                    const { name, surname, phone, id } = item as TItemContactList;
+                    return <ItemListContact
+                        key={id}
+                        name={name}
+                        surname={surname}
+                        phone={phone}
+                        id={id}
+                        deleteContact={deleteContact}
+                        stateViewForm={stateViewForm}
+                        getDataItem={getDataItem}
+                    />
+                })
+            }
+        </ul>
+        <button
+            className="add__contact"
+            onClick={
+                (): void => {
+                    stateViewForm('add');
+                    resetUpdateData();
+                }
+            }
+        >
+            add contact
+        </button>
+    </div>
 }
 
 export { ListContact }
